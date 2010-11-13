cb4c827567c90a0eaeb675fa8f1e88b17e79d74c
jonsowman <jon.sowman@gmail.com>
2010-11-13 11:54:38 +0000

    Added type definition for checksum_type

---
+++ librtty/types.h
@@ -0,0 +1,16 @@
+/*
+  rtty.h
+  A library for outputting RTTY signals
+  
+  CU Spaceflight 2010
+*/
+
+#ifndef types_h
+#define types_h
+
+enum checksum_type {
+    CHECKSUM_NONE,
+    CHECKSUM_CRC16
+};
+
+#endif
