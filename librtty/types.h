aad81faaa8954be2cb3960cceec69b9e3d3b4c56
Jon Sowman <jon.sowman@gmail.com>
2010-12-27 21:24:19 +0000

    Formatting

--- librtty/types.h
+++ librtty/types.h
@@ -2,7 +2,8 @@
    types.h
    Copyright Jon Sowman 2010
 
-   This file is part of the Ferret/librtty project, an Arduino based high altitude balloon tracker library for outputting RTTY signals
+   This file is part of the Ferret/librtty project, an Arduino based
+   high altitude balloon tracker library for outputting RTTY signals.
 
    CU Spaceflight 2010
    http://www.cuspaceflight.co.uk
